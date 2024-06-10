756fec016cbd102e59c2a2472ca1a8f0cbec1e38
Guillermo Galan <ggo@guillego.com>
2024-06-10 17:44:30 +0200

    complete quiz1

--- exercises/quiz1.rs
+++ exercises/quiz1.rs
@@ -13,10 +13,16 @@
 //
 // No hints this time ;)
 
-// I AM NOT DONE
 
 // Put your function here!
-// fn calculate_price_of_apples {
+fn calculate_price_of_apples(n_apples: i32) -> i32 {
+    let apple_cost = 2;
+    if n_apples <= 40 {
+        n_apples*apple_cost
+    } else {
+        n_apples
+    }
+}
 
 // Don't modify this function!
 #[test]
